f9fbed4316b86a1adc101a3986ea02beeb738317
BERNARDO ALTAMIRANO GARCIA <baltamir@itam.mx>
2018-10-17 13:00:46 -0500

    commit

--- AplicaciónWebBD/AltaPagos.aspx
+++ AplicaciónWebBD/AltaPagos.aspx
@@ -6,30 +6,16 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
-    <style type="text/css">
-        .auto-style1 {
-            width: 95%;
-            z-index: 1;
-            left: 23px;
-            top: 163px;
-            position: absolute;
-            height: 27px;
-        }
-        .auto-style2 {
-            height: 23px;
-        }
-    </style>
-</head>
+    </head>
 <body>
     <form id="form1" runat="server">
     <div style="background-color: #C0C0C0; height: 439px;">
     
         <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 65px; top: 38px; position: absolute" Text="Clientes"></asp:Label>
-<<<<<<< HEAD
-        <asp:DropDownList ID="DropDownList1" runat="server" style="z-index: 1; left: 194px; top: 35px; position: absolute; height: 16px; width: 166px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True">
-=======
+
+
         <asp:DropDownList ID="DropDownList1" runat="server" style="z-index: 1; left: 194px; top: 35px; position: absolute; height: 16px; width: 166px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
->>>>>>> b9efeb8b9d6646a8bfcee35027deb91ea4ee19e3
+
         </asp:DropDownList>
         <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 67px; top: 94px; position: absolute" Text="Claves Pedidos" Visible="False"></asp:Label>
     
--- AplicaciónWebBD/AltaPagos.aspx.cs
+++ AplicaciónWebBD/AltaPagos.aspx.cs
@@ -12,14 +12,9 @@ public partial class AltasEmpleados : System.Web.UI.Page
     string cadSql;
     Comunes comunes = new Comunes();
 
-<<<<<<< HEAD
     DataSet DsClientes = new DataSet();
     DataSet DsPedidos = new DataSet();
-
-=======
-    DataSet dsClientes = new DataSet();
     
->>>>>>> b9efeb8b9d6646a8bfcee35027deb91ea4ee19e3
     protected void Page_Load(object sender, EventArgs e)
     {
         if(!IsPostBack)
@@ -28,23 +23,24 @@ public partial class AltasEmpleados : System.Web.UI.Page
             GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
 
             cadSql = "select u.nombre, u.RFC from PCUsuarios u, PCClientes c, PCPedidos p where u.RFC=c.RFC and c.RFC=p.RFCC and p.RFCE='" + rfc + "'";
-            GestorBD.consBD(cadSql, dsClientes, "Clientes");
+            GestorBD.consBD(cadSql, DsClientes, "Clientes");
 
             DropDownList1.Items.Add(" ");
-            foreach (DataRow fila in dsClientes.Tables["Clientes"].Rows)
+            foreach (DataRow fila in DsClientes.Tables["Clientes"].Rows)
                 DropDownList1.Items.Add(new ListItem(fila["nombre"].ToString(), fila["RFC"].ToString()));
         }
         
 
     }
 
-<<<<<<< HEAD
 
     protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
     {
+        Label2.Visible = true;
+        DropDownList2.Visible = true;
         GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
-        cadSql = "select FolioP from  PCPedidos p, PCClientes c, PCEmpleados e where p.RFCC = '"+
-            DropDownList1.SelectedValue+"' and p.RFCC = '"+ Session["rfc"].ToString()+"'";
+        cadSql = "select distinct p.FolioP from  PCPedidos p, PCClientes c, PCEmpleados e where p.RFCC = '"+
+            DropDownList1.SelectedValue+"' and p.RFCE = '"+ Session["rfc"].ToString()+"'";
         GestorBD.consBD(cadSql, DsPedidos, "Pedidos");
         comunes.cargaDDL(DropDownList2, DsPedidos, "Pedidos", "FolioP");
         Session["DsPedidos"] = DsPedidos;
@@ -56,18 +52,14 @@ public partial class AltasEmpleados : System.Web.UI.Page
         //la fecha del pedido, su total a pagar, la suma acumulada de los montos de los 
         //    pagos que se han realizado para el mismo y el saldo que resta por cubrir.
         //    Si no tiene pagos, muestra un mensaje adecuado.
+        Table1.Visible = true;
 
-      GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
-        cadSql = "select fechaPed, sum(Monto) as sum, saldoCli, saldoFacs from  PCPedidos p, PCClientes c, PCEmpleados e where p.RFCC = '" +
-            DropDownList1.SelectedValue + "' and p.RFCC = '" + Session["rfc"].ToString() + "'";
-        GestorBD.consBD(cadSql, DsPedidos, "Pedidos");
-        comunes.cargaDDL(DropDownList2, DsPedidos, "Pedidos", "FolioP");
-=======
-    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        Label2.Visible = true;
-        DropDownList2.Visible = true;
-        
->>>>>>> b9efeb8b9d6646a8bfcee35027deb91ea4ee19e3
+        //GestorBD = (GestorBD.GestorBD)Session["GestorBD"];
+        //cadSql = "select fechaPed, sum(Monto) as sum, saldoCli, saldoFacs from  PCPedidos p, PCClientes c, PCEmpleados e where p.RFCC = '" +
+        //    DropDownList1.SelectedValue + "' and p.RFCC = '" + Session["rfc"].ToString() + "'";
+        //GestorBD.consBD(cadSql, DsPedidos, "Pedidos");
+        //comunes.cargaDDL(DropDownList2, DsPedidos, "Pedidos", "FolioP");
+
     }
+    
 }
